81b6a33b38822b3e249272185e777cf17c059a94
Pau Núñez <linkinpau.97@gmail.com>
2020-02-10 20:09:33 +0100

    10/02/2020 - Legal Texts (Z)

--- src/compontents/LegalTexts.js
+++ src/compontents/LegalTexts.js
@@ -5,10 +5,17 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "./LegalTexts.css";
 
 const LegalTexts = props => {
+  /* Give content room above so it doesnt collide with TitleBar */
+  useEffect(() => {
+    document.getElementById("content-ref").style.paddingTop =
+      (document.getElementById("title-bar-ref").offsetHeight + 16).toString() +
+      "px";
+  }, []);
+
   // Title Bar =~ NavBar for this page
-  const TitleBar = () => (
+  const TitleBar = props => (
     <div style={{ position: "fixed" }}>
-      <div className="title-bar">
+      <div className="title-bar" id="title-bar-ref">
         <div className="title-bar-item">
           <Link to="/" onClick={() => window.scrollTo(0, 0)}>
             <i className="fa fa-chevron-left" aria-hidden="true"></i>
@@ -16,7 +23,7 @@ const LegalTexts = props => {
           </Link>
         </div>
         <div className="title-bar-item">
-          <h3>aaaIaaa</h3>
+          <h3>{props.lang.footer.legal[props.type]}</h3>
         </div>
         <div className="title-bar-item"> </div>
       </div>
@@ -27,7 +34,10 @@ const LegalTexts = props => {
     <LangContext.Consumer>
       {([lang, _]) => (
         <div className="legaltexts-wrapper">
-          <TitleBar />
+          <TitleBar lang={lang} type={props.type} />
+          <div className="legaltexts-content-wrapper" id="content-ref">
+            {content[props.type]}
+          </div>
         </div>
       )}
     </LangContext.Consumer>
@@ -35,3 +45,29 @@ const LegalTexts = props => {
 };
 
 export default LegalTexts;
+
+/* ====== Content of the Page ====== */
+// _0 --> legal
+// _1 --> privacy
+// _2 --> cookies
+// content = [_0, _1, _2]
+const _0 = props => (
+  <React.Fragment>
+    <p>
+      En cumplimiento del artículo 10 de la Ley 34/2002, de 11 de julio, de
+      Servicios de la Sociedad de la Información y del Comercio Electrónico
+      (LSSICE), Preintser Multiservicios, S.L.U. (en adelante, “Reformas
+      Preintser”), Responsable de esta web, pone a disposición de los Usuarios
+      la presente información, para definir sus Condiciones de Uso.
+    </p>
+    <p>
+      Adicionalmente a los contenidos aquí expuestos, los aspectos específicos
+      relacionados con la protección de los datos personales y la privacidad de
+      los usuarios de esta web se desarrollan en las páginas de Política de
+      Privacidad y Política de Cookies.
+    </p>
+  </React.Fragment>
+);
+const _1 = props => "Hola";
+const _2 = props => "Hola";
+const content = [<_0 />, <_1 />, <_2 />];
